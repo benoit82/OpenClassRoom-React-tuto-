32258c66a9c3cad20438fa26d43e26f77c2a8e26
Benoit <benoit.dubus@outlook.fr>
2020-03-09 12:44:41 +0100

    Chapiter : "Ecrivez des fonctions pures"

--- memory/src/index.js
+++ memory/src/index.js
@@ -1,11 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+/** Chapitre : "Ecrivez des fonctions pures"
+function CoolComponent({ prop, toto }) {
+  return (
+    <p>
+      {toto} is {prop} cool !
+    </p>
+  );
+}
+ReactDOM.render(
+    <CoolComponent toto="toto" prop="so" />,
+    document.getElementById("root")
+    );
+*/
 
+ReactDOM.render(<App />, document.getElementById("root"));
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
