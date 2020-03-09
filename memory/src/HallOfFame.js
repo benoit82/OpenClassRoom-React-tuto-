fcb106be4f6a2b736393448f09b2111feb1e2933
Benoit <benoit.dubus@outlook.fr>
2020-03-09 15:54:39 +0100

    Chapitre : "Manipulez des listes de composants"' - fin

--- memory/src/HallOfFame.js
+++ memory/src/HallOfFame.js
@@ -1,22 +1,28 @@
-import React from 'react'
+import React from "react";
 
-import './HallOfFame.css'
+import "./HallOfFame.css";
 
 const HallOfFame = ({ entries }) => (
   <table className="hallOfFame">
     <tbody>
-      <tr>FIXME</tr>
+      {entries.map(({ id, date, name, guesses, player }) => (
+        <tr key={id}>
+          <td className="date">{date}</td>
+          <td className="guesses">{guesses}</td>
+          <td className="player">{player}</td>
+        </tr>
+      ))}
     </tbody>
   </table>
-)
+);
 
-export default HallOfFame
+export default HallOfFame;
 
 // == Internal helpers ==============================================
 
 export const FAKE_HOF = [
-  { id: 3, guesses: 18, date: '10/10/2017', player: 'Jane' },
-  { id: 2, guesses: 23, date: '11/10/2017', player: 'Kevin' },
-  { id: 1, guesses: 31, date: '06/10/2017', player: 'Louisa' },
-  { id: 0, guesses: 48, date: '14/10/2017', player: 'Marc' },
-]
+  { id: 3, guesses: 18, date: "10/10/2017", player: "Jane" },
+  { id: 2, guesses: 23, date: "11/10/2017", player: "Kevin" },
+  { id: 1, guesses: 31, date: "06/10/2017", player: "Louisa" },
+  { id: 0, guesses: 48, date: "14/10/2017", player: "Marc" }
+];
